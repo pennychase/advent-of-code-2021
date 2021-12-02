dc3c1e54dc6b63f1f7300484049ffdfb259d02bb
Penny Chase <mpchase@mac.com>
2021-12-02 17:26:23 -0500

    Day 2 - Part 2: Used lenses to handle nested record

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Day2 where
 
+import Control.Lens
+import Control.Lens.Operators
+import Data.List (foldl')
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Data.Void
@@ -14,6 +18,8 @@ data Position
         }
     deriving (Show, Eq)
 
+makeLenses ''Position
+
 data Course 
     = Course
         { _position :: Position
@@ -21,31 +27,48 @@ data Course
         }
     deriving (Show, Eq)
 
+makeLenses ''Course
+
 data Direction = Forward | Up | Down
     deriving Show
 
 data Move = Move Direction Int
     deriving Show
 
-move1 :: Move -> Position -> Position
-move1 m pos@Position{ .. } =
-    case m of
-        Move Forward n -> pos { _horiz = _horiz + n }
-        Move Up n -> pos { _depth = _depth - n }
-        Move Down n -> pos { _depth = _depth + n }
-
--- move2 :: Move -> Course -> Course
--- move2 m = course@Course { .. }
+-- Original move1 using RecordWildcards and at patterns
+-- move1 :: Move -> Position -> Position
+-- move1 m pos@Position{ .. } =
 --     case m of
---         Move Forward n -> course { position = Position { horiz = horiz + x, depth = aim * n }}
---         Move Up n -> course { _aim = _aim - n }
---         Move Down n -> course { _aim  = _aim +  n }
+--         Move Forward n -> pos { _horiz = _horiz + n }
+--         Move Up n -> pos { _depth = _depth - n }
+--         Move Down n -> pos { _depth = _depth + n }
+
+-- Using lenses
+-- +~ adds a value to the lens; -~ subtracts a value from the lens
+move1 :: Move -> Position -> Position
+move1 m pos =
+    case m of
+        Move Forward n -> pos & horiz +~ n
+        Move Up n -> pos & depth -~ n
+        Move Down n -> pos & depth +~ n
+
+move2 :: Move -> Course -> Course
+move2 m course =
+    case m of
+        Move Forward n -> course & position . horiz +~ n & position . depth +~ (course^.aim * n)
+        Move Up n -> course & aim -~ n 
+        Move Down n -> course & aim +~ n
 
 
 part1 :: [Move] -> Int 
-part1 moves = _depth pos * _horiz pos
+part1 moves = pos ^. depth * pos ^. horiz 
     where
         pos = foldr move1 (Position 0 0) moves
+
+part2 :: [Move] -> Int 
+part2 moves = (course ^. position . depth) * (course ^. position . horiz)
+    where
+        course = foldl' (flip move2) (Course (Position 0 0) 0) moves
 
 -- Parser
 type Parser a = Parsec Void String a
@@ -90,5 +113,10 @@ main = do
     contents <- readFile "./data/day2-input.txt"
     case runParser parseMoves "" contents of
         Left err -> putStrLn "Unable to parse input"
-        Right moves -> putStrLn $ "Part 1: " <> show (part1 moves)
+        Right moves -> do
+            putStrLn $ "Part 1: " <> show (part1 moves)
+            putStrLn $ "Part 2: " <> show (part2 moves)
 
+-- Test Data
+testData :: String
+testData = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
